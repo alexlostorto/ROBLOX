913ff8c82e2ca41d78f9ef351db3b4d084fc6cf9
Alex Lo Storto <lostorto.business@gmail.com>
2023-01-31 20:23:17 +0000

    Updated ReadMe and Player Height script

--- Player_Height.lua
+++ Player_Height.lua
@@ -5,6 +5,8 @@ wait(1)
 local Player = game.Players.LocalPlayer
 local Char = Player.Character
 
+local offsetY = -76.99
+
 game:GetService("RunService").Stepped:Connect(function()
-	Text.Text = "Current Altitude: "..math.floor((Char.HumanoidRootPart.Position.Y-76.99)/3)
+	Text.Text = "Current Altitude: "..math.floor((Char.HumanoidRootPart.Position.Y + offsetY) / 3)
 end)
